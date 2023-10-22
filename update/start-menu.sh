372e8fddd98bf73696ef89a65a582788e7161978
Fikrii29 <148403723+Fikrii29@users.noreply.github.com>
2023-10-23 01:32:58 +0700

    Update start-menu.sh

--- update/start-menu.sh
+++ update/start-menu.sh
@@ -130,5 +130,3 @@ fi
 # echo -e " Xray/V2Ray              :$RED [Error] $NC"
 # fi
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
-echo -e "$BRED----------- Mod By LIZSVR -----------"
-echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
